5e4385f1c82f556a4e91237eb00fbc909d90702c
Narayan221 <narayan.dutt@ditstek.com>
2025-09-19 14:57:24 +0530

    none

--- app.py
+++ app.py
@@ -146,4 +146,4 @@ async def validate_session(data: dict):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("app:app", host="0.0.0.0", port=port, reload=False)
+    uvicorn.run("backend.app:app", host="0.0.0.0", port=port, reload=False)
